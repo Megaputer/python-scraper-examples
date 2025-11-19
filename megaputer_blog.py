8397b309b274d2c8604fb01e6bd66ef61dd9627a
yatmanov <yatmanov@megaputer.ru>
2025-11-19 17:13:39 +0300

    Fix megaputer_blog scraper

--- megaputer_blog.py
+++ megaputer_blog.py
@@ -1,13 +1,12 @@
 """
 ### Megaputer blog scraper
 
-Downloads articles from https://megaputer.ru/blog
+Downloads articles from https://megaputer.com/blog
 
 #### Columns
 
 | Name        | Type      | Description
 | :---------- | :-------- | :----------
-| `Author`    | String    | The author of a post
 | `Published` | Date/Time | Publication date
 
 """
@@ -21,14 +20,11 @@ from utils import column_types, default_datetime_format, InternetNode
 
 
 def main():
-    columns = {
-        'Author': column_types['String'],
-        'Published': column_types['Date/Time'],
-    }
+    columns = {'Published': column_types['Date/Time']}
     with InternetNode(__doc__, columns) as node, requests.Session() as s:
 
         # walk through blog pages and collect post links
-        url = 'https://megaputer.ru/blog'
+        url = 'https://megaputer.com/blog'
         posts = {}
         while True:
             if node.is_cancelled():
@@ -41,14 +37,12 @@ def main():
 
             for article_el in dom.xpath('//article'):
                 link = article_el.xpath('.//a[@rel="bookmark"]/@href')[0]
-                author = article_el.xpath('.//span[contains(@class, "author")]//text()')[0]
                 _published = article_el.xpath('.//time/@datetime')[0]
 
                 # convert @datetime format to PA: '2020-12-10 12:00:54' > '12/10/2020 01:00:54 pm'
                 _parsed = datetime.strptime(_published, '%Y-%m-%d %H:%M:%S')
                 published = _parsed.strftime(default_datetime_format)
-
-                posts[link] = (author, published)
+                posts[link] = published
 
             try:
                 url = dom.xpath('//nav//a[contains(@class, "next")]/@href')[0]
@@ -59,7 +53,7 @@ def main():
         logging.info(f'Found next blog posts: {posts}')
         # download collected posts
         c = 0
-        for url, (author, published) in posts.items():
+        for url, published in posts.items():
             if node.is_cancelled() or c >= node.rows_limit:
                 logging.info('Node execution is cancelled')
                 break
@@ -69,13 +63,7 @@ def main():
                     resp.raise_for_status()
                     dom = lxml.html.fromstring(resp.content)
 
-                if not dom.xpath('//main'):
-                    # new article layout
-                    texts = dom.xpath(
-                        '//section[not(ancestor::footer) and not(.//header) and not(.//h1) and not(.//h3)]//text()'
-                    )
-                else:
-                    texts = dom.xpath('//main//div[@itemprop="text"]//text()')
+                texts = dom.xpath('//main/section[contains(@class,"l-section") and not(contains(@class, "for_sharing")) and not(contains(@class, "for_related"))]//text()')
 
                 try:
                     title = dom.xpath('//title/text()')[0].strip()
@@ -86,7 +74,7 @@ def main():
                 logging.warning(f'Failed to download post "{url}": {exc}')
                 continue
 
-            node.insert(url, title, ''.join(texts), Author=author, Published=published)
+            node.insert(url, title, ''.join(texts), Published=published)
             c += 1
 
 
